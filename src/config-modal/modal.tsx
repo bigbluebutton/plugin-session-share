dc711601d9a6ffa4da0f909392ec573e7ab6a876
Anton Georgiev <antobinary@users.noreply.github.com>
2024-07-19 14:09:46 -0400

    Packaging, CI etc from template (#8)
    * packaging, CI, etc from template
    
    * [merge-with-template-fix-lint] fixing lint errors (#9)
    
    ---------
    
    Co-authored-by: Guilherme Pereira Leme <69865537+GuiLeme@users.noreply.github.com>

--- src/config-modal/modal.tsx
+++ src/config-modal/modal.tsx
@@ -3,8 +3,8 @@ import * as React from 'react';
 import { useState } from 'react';
 import * as ReactModal from 'react-modal';
 import './style.css';
-import QRCode from "react-qr-code";
-import {CopyToClipboard} from 'react-copy-to-clipboard';
+import QRCode from 'react-qr-code';
+import { CopyToClipboard } from 'react-copy-to-clipboard';
 
 interface ShareModalProps {
     isOpen: boolean;
@@ -13,87 +13,97 @@ interface ShareModalProps {
     onConfirm: (shareType: string) => void;
 }
 
-export const ShareModal: React.FC<ShareModalProps> = ({ isOpen, newJoinUrl, onClose, onConfirm }) => {
-    const [shareType, setShareType] = useState('shareSession');
-    const [loading, setLoading] = useState(false);
-    
-    // This is required for accessibility reasons.
-    // You can alternatively set it on your root app component during app initialization.
-    ReactModal.setAppElement('#app'); // Adjust '#root' to match your application's mount node ID.
+export function ShareModal({
+  isOpen, newJoinUrl, onClose, onConfirm,
+}: ShareModalProps) {
+  const [shareType, setShareType] = useState('shareSession');
+  const [loading, setLoading] = useState(false);
 
-    React.useEffect(() => {
-        if(isOpen) {
-            setLoading(false);
-        }
-    }, [isOpen]);
-    return (
-        <ReactModal
-            isOpen={isOpen}
-            onRequestClose={onClose}
-            contentLabel="Share Options Modal"
-            className="plugin-modal session-share-plugin"
-            overlayClassName="modal-overlay"
-        >
-        <div
-            style={{
-                width: '100%', height: '100%', alignItems: 'center', display: 'flex', flexDirection: 'column',
-            }}
-            >
-            <h2>Share Options</h2>
-            { 
+  // This is required for accessibility reasons.
+  // You can alternatively set it on your root app component during app initialization.
+  ReactModal.setAppElement('#app'); // Adjust '#root' to match your application's mount node ID.
+
+  React.useEffect(() => {
+    if (isOpen) {
+      setLoading(false);
+    }
+  }, [isOpen]);
+  return (
+    <ReactModal
+      isOpen={isOpen}
+      onRequestClose={onClose}
+      contentLabel="Share Options Modal"
+      className="plugin-modal session-share-plugin"
+      overlayClassName="modal-overlay"
+    >
+      <div
+        style={{
+          width: '100%', height: '100%', alignItems: 'center', display: 'flex', flexDirection: 'column',
+        }}
+      >
+        <h2>Share Options</h2>
+        {
             /** Join URL not created */
-            ! newJoinUrl ?
+            !newJoinUrl
+              ? (
                 <>
-                <form>
-                    <label>
-                        <input
-                            type="radio"
-                            name="shareType"
-                            value="shareSession"
-                            checked={shareType === 'shareSession'}
-                            onChange={() => setShareType('shareSession')}
-                        />
-                        Share my session
+                  <form>
+                    <label htmlFor="share-mine">
+                      <input
+                        type="radio"
+                        name="shareType"
+                        id="share-mine"
+                        value="shareSession"
+                        checked={shareType === 'shareSession'}
+                        onChange={() => setShareType('shareSession')}
+                      />
+                      Share my session
                     </label>
-                    <label>
-                        <input
-                            type="radio"
-                            name="shareType"
-                            value="inviteUsers"
-                            checked={shareType === 'inviteUsers'}
-                            onChange={() => setShareType('inviteUsers')}
-                        />
-                        Invite other users
+                    <label htmlFor="invite-others">
+                      <input
+                        type="radio"
+                        name="shareType"
+                        id="invite-others"
+                        value="inviteUsers"
+                        checked={shareType === 'inviteUsers'}
+                        onChange={() => setShareType('inviteUsers')}
+                      />
+                      Invite other users
                     </label>
-                </form>
-                <div className="buttons-container">
-                    <button disabled={loading} onClick={() => { onConfirm(shareType); setLoading(true); }}>Confirm</button>
-                    <button disabled={loading} onClick={onClose}>Close</button>
-                </div>
-                </> : null 
+                  </form>
+                  <div className="buttons-container">
+                    <button type="button" disabled={loading} onClick={() => { onConfirm(shareType); setLoading(true); }}>Confirm</button>
+                    <button type="button" disabled={loading} onClick={onClose}>Close</button>
+                  </div>
+                </>
+              ) : null
             }
-            { 
+        {
             /** Join URL available */
-            newJoinUrl ?
+            newJoinUrl
+              ? (
                 <>
-                <CopyToClipboard text={newJoinUrl} onCopy={onClose}>
+                  <CopyToClipboard text={newJoinUrl} onCopy={onClose}>
                     <div className="copy-button-wrapper">
-                        <i className="icon-bbb-copy"></i> Copy
+                      <i className="icon-bbb-copy" />
+                      {' '}
+                      Copy
                     </div>
-                </CopyToClipboard>
-                <QRCode
+                  </CopyToClipboard>
+                  <QRCode
                     size={128}
-                    style={{ height: "128", maxWidth: "128", width: "128" }}
+                    style={{ height: '128', maxWidth: '128', width: '128' }}
                     value={newJoinUrl}
-                    viewBox={`0 0 128 128`}
-                />
+                    viewBox="0 0 128 128"
+                  />
 
-                <div className="buttons-container">
-                    <button onClick={onClose}>Close</button>
-                </div>
-                </> : null 
+                  <div className="buttons-container">
+                    <button type="button" onClick={onClose}>Close</button>
+                  </div>
+                </>
+              ) : null
             }
-        </div>
-      </ReactModal>
-    );
-};
+      </div>
+    </ReactModal>
+  );
+}
